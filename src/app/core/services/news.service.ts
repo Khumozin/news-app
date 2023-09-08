caa6816982b36cb3818c03d1d1414ea4a5377b16
Khumozin <Khumomogorosi@gmail.com>
2023-09-08 12:52:22 +0200

    feat(app): implement news app

---
+++ src/app/core/services/news.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { environment } from '../../../environments/environment.development';
+import { NewsApiOkResponse, SearchParam } from '../../shared/models';
+
+function buildParams(searchParams: SearchParam) {
+  let params = new HttpParams();
+  const paramNames = Object.keys(searchParams);
+
+  paramNames.forEach((param, index) => {
+    params =
+      index === 0
+        ? params.set(param, searchParams[param as keyof SearchParam])
+        : params.append(param, searchParams[param as keyof SearchParam]);
+  });
+
+  return params;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class NewsService {
+  private readonly url = environment.newApiUrl;
+  private readonly http = inject(HttpClient);
+
+  getArticles(searchParams: SearchParam): Observable<NewsApiOkResponse> {
+    const params = buildParams(searchParams);
+
+    return this.http.get<NewsApiOkResponse>(`${this.url}`, { params });
+  }
+}
