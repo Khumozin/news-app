733001f4c350796c0eb64faa8943ce0555fec271
Khumo Mogorosi <KMogorosi@toyota.co.za>
2023-09-22 14:06:14 +0200

    test(app): add cypress e2e testing
    add cypress e2e testing

---
+++ cypress/e2e/spec.cy.ts
@@ -0,0 +1,214 @@
+const API_RESPONSE = {
+  status: 'ok',
+  totalResults: 163,
+  articles: [
+    {
+      source: {
+        id: null,
+        name: 'Forbes',
+      },
+      author:
+        'Sam Lyman, Contributor, \n Sam Lyman, Contributor\n https://www.forbes.com/sites/samlyman/',
+      title:
+        'From Venture Capital To Wall Street, A New Paradigm Emerges: BTC = ESG',
+      description:
+        "Could BTC = ESG? According to new research, the answer is a resounding yes. Here's why investors are taking a second look at bitcoin through the lens of ESG.",
+      url: 'https://www.forbes.com/sites/digital-assets/2023/09/21/from-venture-capital-to-wall-street-a-new-paradigm-emerges-btc--esg/',
+      urlToImage:
+        'https://imageio.forbes.com/specials-images/imageserve/650bc2d07c251b88f0500006/0x0.jpg?format=jpg&width=1200',
+      publishedAt: '2023-09-21T11:00:00Z',
+      content:
+        "ABILENE, TX - Today, renewables like wind and solar power more than 50% of bitcoin mining activity. ... [+] The world's leading cryptocurrency is creating new markets for clean energy, which has inve… [+7708 chars]",
+    },
+    {
+      source: {
+        id: null,
+        name: 'Forbes',
+      },
+      author:
+        'Mickey Koss, Contributor, \n Mickey Koss, Contributor\n https://www.forbes.com/sites/mikalkoss/',
+      title: 'Grayscale’s Crypto Fund Discounts Extend Far Beyond Bitcoin',
+      description:
+        'The world’s largest digital asset manager offers 17 products tracking ether, bitcoin cash, chainlink, and many others that trade far away from the value of their underlying collateral. Should investors see them as trading opportunities?',
+      url: 'https://www.forbes.com/sites/digital-assets/2023/09/21/grayscales-crypto-fund-discounts-extend-far-beyond-bitcoin/',
+      urlToImage:
+        'https://imageio.forbes.com/specials-images/imageserve/650b4291c1b2cc280fc13f07/0x0.jpg?format=jpg&width=1200',
+      publishedAt: '2023-09-21T10:30:00Z',
+      content:
+        'Grayscale offers more than a dozen products besides its flagship bitcoin trust. Most trade at heavy discounts. Photographer: Gabby Jones/Bloomberg\r\n© 2023 Bloomberg Finance LP\r\nGrayscale Offers A Lot… [+6710 chars]',
+    },
+    {
+      source: {
+        id: null,
+        name: 'CoinDesk',
+      },
+      author: 'Omkar Godbole',
+      title:
+        'Bitcoin Bulls Missing in Action After Mt. Gox Delays BTC Repayments',
+      description:
+        'Rumors of Mt. Gox mulling a delay have been doing the rounds and likely catalyzed the recent bitcoin price bounce.',
+      url: 'https://www.coindesk.com/markets/2023/09/21/bitcoin-bulls-missing-in-action-after-mt-gox-delays-btc-repayments/',
+      urlToImage:
+        'https://www.coindesk.com/resizer/72JbR7q9fVTp1Hfp6L3mbZPwrjc=/1200x628/center/middle/cloudfront-us-east-1.images.arcpublishing.com/coindesk/LMBZWLENG5H6XAQ7KNDJCFOP2I.jpg',
+      publishedAt: '2023-09-21T09:03:36Z',
+      content:
+        '<ul><li>Bitcoin stays below the 50-day simple moving average after the defunct exchange Mt. Got delays bankruptcy-related creditor repayments.\r\n</li><li>The delay was expected and priced in.\r\n</li></… [+3003 chars]',
+    },
+    {
+      source: {
+        id: null,
+        name: 'Yahoo Entertainment',
+      },
+      author: 'Shaurya Malwa',
+      title:
+        "Bitcoin’s 87% Drop in 2021 Was Caused by Sam Bankman-Fried's Alameda, Ex-Employee Claims",
+      description:
+        "An ex-Alameda employee claims a trader at the firm punched in a wrong decimal which led to bitcoin's 87% drop on Binance.US in 2021.",
+      url: 'https://finance.yahoo.com/news/bitcoin-87-drop-2021-caused-073440072.html',
+      urlToImage:
+        'https://s.yimg.com/ny/api/res/1.2/FDgF9ClgNYk56oxqTzV6Mg--/YXBwaWQ9aGlnaGxhbmRlcjt3PTEyMDA7aD05MDA-/https://media.zenfs.com/en/coindesk_75/315e5fb2bcd93d4de7273bfd64439aa6',
+      publishedAt: '2023-09-21T07:34:40Z',
+      content:
+        'Disgraced trading firm Alameda Research was behind the hiccup that caused bitcoin (BTC) prices to temporarily drop over 87% in 2021, as per an ex-employee who has started to reveal the inside working… [+1789 chars]',
+    },
+    {
+      source: {
+        id: null,
+        name: 'Biztoc.com',
+      },
+      author: 'cryptoslate.com',
+      title:
+        'Mt. Gox trustee extends hacked exchange’s Bitcoin repayment deadline to October 2024',
+      description:
+        'The trustee of the hacked crypto exchange Mt. Gox has extended the repayment deadline by a year to Oct. 31, 2024. A Sept. 21 letter published by the Rehabilitation Trustee stated that the extension was necessary to complete all the repayment processes before …',
+      url: 'https://biztoc.com/x/be84242985cd424b',
+      urlToImage: 'https://c.biztoc.com/p/be84242985cd424b/s.webp',
+      publishedAt: '2023-09-21T07:36:08Z',
+      content:
+        'The trustee of the hacked crypto exchange Mt. Gox has extended the repayment deadline by a year to Oct. 31, 2024.A Sept. 21 letter published by the Rehabilitation Trustee stated that the extension wa… [+275 chars]',
+    },
+    {
+      source: {
+        id: null,
+        name: 'CoinDesk',
+      },
+      author: 'Shaurya Malwa',
+      title:
+        "Bitcoin’s 87% Drop in 2021 Was Caused by Sam Bankman-Fried's Alameda, Ex-Employee Claims",
+      description:
+        "An ex-Alameda employee claims a trader at the firm punched in a wrong decimal which led to bitcoin's 87% drop on Binance.US in 2021.",
+      url: 'https://www.coindesk.com/markets/2023/09/21/bitcoins-87-drop-in-2021-was-caused-by-sam-bankman-frieds-alameda-ex-employee-claims/',
+      urlToImage:
+        'https://www.coindesk.com/resizer/lsvE6KUEtfuUDEmXIHKUq0TgJaI=/1200x628/center/middle/cloudfront-us-east-1.images.arcpublishing.com/coindesk/27HVBDHQCFDF5CDDDVLB2K33RA.jpg',
+      publishedAt: '2023-09-21T07:34:40Z',
+      content:
+        'Disgraced trading firm Alameda Research was behind the hiccup that caused bitcoin (BTC) prices to temporarily drop over 87% in 2021, as per an ex-employee who has started to reveal the inside working… [+1818 chars]',
+    },
+    {
+      source: {
+        id: null,
+        name: 'CoinDesk',
+      },
+      author: 'Shaurya Malwa',
+      title:
+        "BNB, XRP Lead Slide in Crypto Majors as Mt. Gox's Repayment Delay Fails to Bouy Bitcoin Prices",
+      description:
+        '“We still believe that the chances of further declines are higher for now,” one analyst said.',
+      url: 'https://www.coindesk.com/markets/2023/09/21/bnb-xrp-lead-slide-in-crypto-majors-as-mt-goxs-repayment-delay-fails-to-bouy-bitcoin-prices/',
+      urlToImage:
+        'https://www.coindesk.com/resizer/AVoWSxFi7b1uBIA1MYMliNI9vqw=/1200x628/center/middle/cloudfront-us-east-1.images.arcpublishing.com/coindesk/FHUONZDL3FF25LQTCH6HDI7DGI.jpg',
+      publishedAt: '2023-09-21T10:21:44Z',
+      content:
+        "<ul><li>Toncoin dropped 8% after jumping nearly 35% last week, while ImmutableX's IMX jumped 30% on Thursday.\r\n</li><li>Optimism's OP token slid 5% after the Optimism Foundation sold 116 million toke… [+2576 chars]",
+    },
+    {
+      source: {
+        id: null,
+        name: 'Techreport.com',
+      },
+      author: 'Nick Dunn',
+      title:
+        'Ripple Price Forecast: New York Regulators Delists Ripple, What’s Next For XRP?',
+      description:
+        'The recent Ripple delisting from the New York roster of permitted cryptocurrencies raises concern within the crypto market. Despite this negative news, XRP is showing strong performance in the market....\nThe post Ripple Price Forecast: New York Regulators Del…',
+      url: 'https://techreport.com/crypto-news/ripple-price-forecast-new-york-regulators-delists-ripple-whats-next-for-xrp/',
+      urlToImage:
+        'https://techreport.com/wp-content/uploads/2023/09/ripple-labs-logo.png',
+      publishedAt: '2023-09-21T01:30:39Z',
+      content:
+        'The recent Ripple delisting from the New York roster of permitted cryptocurrencies raises concern within the crypto market. Despite this negative news, XRP is showing strong performance in the market… [+5942 chars]',
+    },
+    {
+      source: {
+        id: null,
+        name: 'Yahoo Entertainment',
+      },
+      author: 'Parikshit Mishra',
+      title: 'Mt. Gox Pushes Repayment Deadline by a Year',
+      description:
+        'The defunct crypto exchange has delayed the deadline for repayments to Oct. 31, 2024.',
+      url: 'https://finance.yahoo.com/news/mt-gox-pushes-repayment-deadline-054120566.html',
+      urlToImage:
+        'https://s.yimg.com/ny/api/res/1.2/AmnT4QVF7mMa9DDt9snkxA--/YXBwaWQ9aGlnaGxhbmRlcjt3PTEyMDA7aD02NzU-/https://media.zenfs.com/en/coindesk_75/c22bc1d6c4f08f8909771ca3994a57e1',
+      publishedAt: '2023-09-21T05:41:20Z',
+      content:
+        'Mt. Gox has pushed back the deadline for its planned repayments by another 12 months, the trustees for firm said on Thursday.\r\nThe defunct crypto exchange had earlier announced plans that the deadlin… [+594 chars]',
+    },
+    {
+      source: {
+        id: null,
+        name: 'Yahoo Entertainment',
+      },
+      author: 'Tom Zuo',
+      title:
+        'Bitcoin, Ether dip; Toncoin leads crypto retreat following hawkish Fed comments',
+      description:
+        'Bitcoin briefly fell below US$26,900 on early Thursday morning in Asia but is now trading above US$27,000. Ether also dropped but held above US$1,600.',
+      url: 'https://finance.yahoo.com/news/bitcoin-ether-dip-toncoin-leads-015341370.html',
+      urlToImage:
+        'https://s.yimg.com/ny/api/res/1.2/0idH67L7In9hRXsA7o7xmA--/YXBwaWQ9aGlnaGxhbmRlcjt3PTEyMDA7aD04MDA-/https://media.zenfs.com/en/forkast_news_articles_672/1c98770acde72a27770167ef3da1215b',
+      publishedAt: '2023-09-21T01:53:41Z',
+      content:
+        'The U.S. Federal Reserve kept interest rates unchanged as expected at its September meeting on Wednesday. Bitcoin briefly fell below US$26,900 in the early hours of the morning after Fed Chair Jerome… [+3616 chars]',
+    },
+  ],
+};
+
+describe('Test App', () => {
+  beforeEach(() => cy.visit('/'));
+
+  it('Visits the initial project page', () => {
+    cy.contains('News App');
+  });
+
+  it('Search for news', () => {
+    const query = 'Bitcoin';
+    const date = new Date();
+    const day = date.getDate();
+
+    cy.intercept('GET', 'https://newsapi.org/v2/everything*', API_RESPONSE);
+
+    cy.get('div').contains(`0 of 0`);
+
+    cy.get('[data-cy="form-input-query"]').type(query);
+
+    cy.get('[data-cy="form-input-date-toggle"]').click();
+
+    cy.get('span').contains(`${day}`).click();
+
+    cy.get('[data-cy="form-input-sort"]')
+      .click()
+      .get('mat-option')
+      .contains('Popularity')
+      .click();
+
+    cy.get('[data-cy="form-submit-button"]').click();
+
+    cy.get('div').contains(`${API_RESPONSE.totalResults}`);
+
+    cy.get('[data-cy="article"]').should(
+      'have.length',
+      API_RESPONSE.articles.length
+    );
+  });
+});
