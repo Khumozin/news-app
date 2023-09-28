deca9627aebb30fdced196196845be7c2c101566
Khumo Mogorosi <KMogorosi@toyota.co.za>
2023-09-28 10:13:41 +0200

    feat(shared): implement loading skeleton
    implement loading skeleton

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { SkeletonRectComponent } from './components/skeleton-rect/skeleton-rect.component';
+import { SkeletonDirective } from './directives/skeleton.directive';
+
 @NgModule({
-  declarations: [],
+  declarations: [SkeletonDirective, SkeletonRectComponent],
   imports: [CommonModule],
+  exports: [SkeletonDirective, SkeletonRectComponent],
 })
 export class SharedModule {}
