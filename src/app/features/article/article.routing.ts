443bb3a60782f22f8dcb7e84fed1d8e1c54aa5f5
Khumo Mogorosi <khumomogorosi@gmail.com>
2024-08-28 19:10:18 +0200

    refactor(articles): implement lazy loading

--- src/app/features/article/article.routing.ts
+++ src/app/features/article/article.routing.ts
@@ -1,10 +1,8 @@
 import { Route } from '@angular/router';
 
-import { ArticlesComponent } from './pages';
-
 export const ARTICLE_ROUTES: Route[] = [
   {
     path: '',
-    component: ArticlesComponent,
+    loadComponent: () => import('./pages').then (c => c.ArticlesComponent),
   },
 ];
