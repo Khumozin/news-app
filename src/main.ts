b735b030599f561d4f193e6fc0af31becfc5fbcf
Khumo Mogorosi <khumomogorosi@gmail.com>
2025-05-15 18:57:37 +0200

    feat: implement dynamic config

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,29 @@
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { inject, provideAppInitializer } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
 
 import { AppComponent } from './app/app.component';
 import { APP_ROUTES } from './app/app.routing';
+import { EnvironmentService } from './app/core/config/environment.service';
 import { networkInterceptor } from './app/core/interceptors';
+
+function initApp() {
+  const environmentService = inject(EnvironmentService);
+
+  return environmentService.load();
+}
 
 bootstrapApplication(AppComponent, {
   providers: [
+    provideAppInitializer(initApp),
     provideAnimationsAsync(),
     provideHttpClient(withInterceptors([networkInterceptor])),
     provideRouter(APP_ROUTES),
+    {
+      provide: EnvironmentService,
+      useValue: new EnvironmentService(),
+    },
   ],
 }).catch(err => console.error(err));
